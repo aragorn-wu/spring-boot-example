ec31d79b22b6611d8dcdf1ac943a27e3d9d742fb
wuqf <wuqingfeng234@163.com>
2017-02-19 13:45:53 +0800

    spring rest can run

---
+++ chapter3-1-1/spring-cloud-rest/src/main/java/com/git/wuqf/SpringCloudRestApplication.java
@@ -0,0 +1,12 @@
+package com.git.wuqf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudRestApplication.class, args);
+	}
+}
---
+++ chapter3-1-1/spring-cloud-rest/src/main/java/com/git/wuqf/model/User.java
@@ -0,0 +1,34 @@
+package com.git.wuqf.model;
+
+/**
+ * Created by wuqf on 17-2-19.
+ */
+public class User {
+    private long id;
+    private String name;
+    private int age;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
